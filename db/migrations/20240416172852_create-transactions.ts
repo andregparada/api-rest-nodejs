25187f71d958ae3ebdd4875f1959920b1388ef0c
André <andregparada@gmail.com>
2024-04-16 15:13:49 -0300

    Realizando queries com Knex

--- db/migrations/20240416172852_create-transactions.ts
+++ db/migrations/20240416172852_create-transactions.ts
@@ -1,7 +1,7 @@
 import type { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.createTable('tansactions', (table) => {
+  await knex.schema.createTable('transactions', (table) => {
     table.uuid('id').primary()
     table.text('title').notNullable()
     table.decimal('amount', 10, 2).notNullable()
@@ -10,5 +10,5 @@ export async function up(knex: Knex): Promise<void> {
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTable('tansactions')
+  await knex.schema.dropTable('transactions')
 }
